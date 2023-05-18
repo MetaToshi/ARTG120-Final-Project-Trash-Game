0854ab0b68647945ac8a26d0f5209f255dd3fcb1
Lumina <Luminadang@gmail.com>
2023-05-18 11:27:06 -0700

    Made Trashcan scene,next logic for placement

--- ARG120 Final - Trash Project/Assets/Characters/Garbage_Person.gd
+++ ARG120 Final - Trash Project/Assets/Characters/Garbage_Person.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 @export var move_speed : float = 100;
+@export var garbage_capacity : int = 10;
 
+var Garbage_Person = load("res://Assets/Characters/Garbage_Person.tscn")
 #func _ready():
 	#update_animation_parameters(starting_direction)
 func _physics_process(_delta):
@@ -16,4 +18,15 @@ func _physics_process(_delta):
 	velocity = input_direction * move_speed
 	
 	move_and_slide()
+	if Input.is_action_pressed("place"):
+		#place_can()
+		print(Garbage_Person.position)
+		
 
+func place_can():
+	#var can = load("res://Assets/Characters/garbage_can.tscn")
+	#var can_instance = can.instantiate()
+	#var can_location = Garbage_Person.position
+	print(Garbage_Person.position)
+	#can_instance.position = can_location
+	#add_child(can_instance)
---
+++ ARG120 Final - Trash Project/Assets/Characters/garbage_can.gd
@@ -0,0 +1,2 @@
+extends CharacterBody2D
+@export var garbage_capacity : int = 5;
