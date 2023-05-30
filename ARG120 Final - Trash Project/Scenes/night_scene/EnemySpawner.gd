fa80a8700c2401a16188a4b444725699216e5977
ethanurl <130177026+ethanurl@users.noreply.github.com>
2023-05-30 10:39:40 -0700

    Added waves

--- ARG120 Final - Trash Project/Scenes/night_scene/EnemySpawner.gd
+++ ARG120 Final - Trash Project/Scenes/night_scene/EnemySpawner.gd
@@ -6,10 +6,13 @@ extends Node2D
 @onready var player = get_tree().get_first_node_in_group("city")
 
 var time = 0
-
+var difficulty = 1
+var wave = 0
 
 func _on_timer_timeout():
 	time += 1
+	wave += 1
+	var howmanyenemy = difficulty
 	var enemy_spawns = spawns
 	for i in enemy_spawns:
 		if time >= i.time_start and time <= i.time_end:
@@ -18,12 +21,31 @@ func _on_timer_timeout():
 			else:
 				i.spawn_delay_counter = 0
 				var new_enemy = load(str(i.enemy.resource_path))
-				var counter = 0
-				while counter < i.enemy_num:
-					var enemy_spawn = new_enemy.instantiate()
-					enemy_spawn.global_position = get_random_position()
-					add_child(enemy_spawn)
-					counter += 1
+				if wave <= 10:
+					while howmanyenemy > 0:
+						print('normalspawn')
+						var counter = 0
+						while counter < i.enemy_num:
+							var enemy_spawn = new_enemy.instantiate()
+							enemy_spawn.global_position = get_random_position()
+							add_child(enemy_spawn)
+							counter += 1
+							howmanyenemy -= 1
+				elif wave > 10:
+					print('waving')
+					while wave >= 0:
+						print('wavecheck')
+						var counter = 0
+						while counter < i.enemy_num:
+							print('funnyspawn')
+							var enemy_spawn = new_enemy.instantiate()
+							enemy_spawn.global_position = get_random_position()
+							add_child(enemy_spawn)
+							counter += 1
+							howmanyenemy -= 1
+						wave -= 1
+						
+					
 
 func get_random_position():
 	var vpr = get_viewport_rect().size * 2.5 #determines how far away enemies spawn
@@ -53,3 +75,6 @@ func get_random_position():
 	var x_spawn = randf_range(spawn_pos1.x, spawn_pos2.x)
 	var y_spawn = randf_range(spawn_pos1.y, spawn_pos2.y)
 	return Vector2(x_spawn, y_spawn)
+
+func increase_difficulty(key):
+	difficulty += key
