949c66ed3bec3a0b2f119d875bf5e962548c22ec
Lumina <Luminadang@gmail.com>
2023-05-29 22:47:07 -0700

    BUY SCENE Work

--- ARG120 Final - Trash Project/Scenes/day_scene/Shop.gd
+++ ARG120 Final - Trash Project/Scenes/day_scene/Shop.gd
@@ -2,13 +2,20 @@ extends Control
 var player;
 
 func _ready():
-	player = get_node("PlayerCat")
+	player = get_node("../PlayerCat")
 
 func _on_playercanupgrade_pressed():
-	var cur = 10
+	var cur = player.get_max_cans()
+	cur += 1
 	player.set_max_cans(cur)
 
 
 
 func _on_playerheldupgrade_pressed():
 	pass # Replace with function body.
+
+
+
+
+func _on_back_to_level_pressed():
+	get_tree().change_scene_to_file("res://Scenes/night_scene/night.tscn")
