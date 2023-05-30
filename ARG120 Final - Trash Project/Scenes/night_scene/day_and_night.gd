e24eb783eaa05395cc47ac63a3250772ef58a2f3
sngbird <Luminadang@gmail.com>
2023-05-30 11:03:10 -0700

    added second scene timer

--- ARG120 Final - Trash Project/Scenes/night_scene/day_and_night.gd
+++ ARG120 Final - Trash Project/Scenes/night_scene/day_and_night.gd
@@ -7,7 +7,7 @@ func _ready():
 	$ColorRect.color.a = 200
 
 func _on_timer_timeout():
-	get_tree().change_scene_to_file("res://Scenes/day_scene/day_scene.tscn")
+	$change_level.start()
 	EnemySpawner.increase_difficulty(1)
 #	
 func _process(delta):
@@ -15,3 +15,7 @@ func _process(delta):
 			
 func change_to_day():
 	$AnimationPlayer.play("night_to_day")
+
+
+func _on_change_level_timeout():
+	get_tree().change_scene_to_file("res://Scenes/day_scene/day_scene.tscn")
