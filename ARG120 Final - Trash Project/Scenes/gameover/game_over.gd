9f3d798d54406beeda4c97a0d62c72c7920d3e74
Lumina <Luminadang@gmail.com>
2023-05-29 23:23:22 -0700

    game_over added

---
+++ ARG120 Final - Trash Project/Scenes/gameover/game_over.gd
@@ -0,0 +1,10 @@
+extends Control
+
+
+
+func _on_onclick_pressed():
+		get_tree().change_scene_to_file("res://Scenes/startmenu/StartMenu.tscn")
+
+
+func _on_timer_timeout():
+		get_tree().change_scene_to_file("res://Scenes/startmenu/StartMenu.tscn")
