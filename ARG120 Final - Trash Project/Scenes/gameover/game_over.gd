f954ecb5a6847e587a7db80f644a60e31374b0b7
Wyatt H <jhawes@ucsc.edu>
2023-06-04 17:27:12 -0700

    making game over call reset scene

--- ARG120 Final - Trash Project/Scenes/gameover/game_over.gd
+++ ARG120 Final - Trash Project/Scenes/gameover/game_over.gd
@@ -3,8 +3,10 @@ extends Control
 
 
 func _on_onclick_pressed():
+		GlobalPlayerVariables.RESET_TO_DEFAULT_VALUES
 		get_tree().change_scene_to_file("res://Scenes/startmenu/StartMenu.tscn")
 
 
 func _on_timer_timeout():
+		GlobalPlayerVariables.RESET_TO_DEFAULT_VALUES
 		get_tree().change_scene_to_file("res://Scenes/startmenu/StartMenu.tscn")
